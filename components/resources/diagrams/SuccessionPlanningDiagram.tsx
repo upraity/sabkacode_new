26cef9f5ae19d5d4db230cd95428d93ccd138e46
Dj <134566303+upraity@users.noreply.github.com>
2026-09-19 20:39:15 +0530

    Update SuccessionPlanningDiagram.tsx

--- components/resources/diagrams/SuccessionPlanningDiagram.tsx
+++ components/resources/diagrams/SuccessionPlanningDiagram.tsx
@@ -3,13 +3,13 @@ import { StepperDiagram } from "./StepperDiagram";
 export function SuccessionPlanningDiagram() {
   return (
     <StepperDiagram
-      steps=[
+      steps={[
         ["Identify", "Critical Roles"],
         ["Define", "Success Profile"],
         ["Assess", "Potential"],
         ["Develop", "Successors"],
         ["Review", "Readiness"],
-      ]
+      ]}
     />
   );
 }
